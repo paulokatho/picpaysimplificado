6de8c2355e27d4d4040473c2e4362dcc7143785b
paulokatho <pkatho@gmail.com>
2023-11-14 13:17:06 +0000

    feat: iniciando configurações do projeto e classes iniciais

---
+++ src/main/java/com/picpaysimplificado/domain/user/User.java
@@ -0,0 +1,40 @@
+package com.picpaysimplificado.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.usertype.UserType;
+
+import java.math.BigDecimal;
+
+@Table(name="users")
+@Entity(name="users")
+@Getter
+@Setter
+@EqualsAndHashCode
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    @Column(unique = true)
+    private String document;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private BigDecimal balance;
+
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+}
---
+++ src/main/java/com/picpaysimplificado/domain/user/UserType.java
@@ -0,0 +1,8 @@
+package com.picpaysimplificado.domain.user;
+
+public enum UserType {
+
+    //USUARIOS NORMAIS E LOJISTAS
+    COMMON,
+    MERCHANT
+}
