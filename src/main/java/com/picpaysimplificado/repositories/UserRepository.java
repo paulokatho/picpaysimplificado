c3919ed94732f46994ade0eb3e8cf2670bdf6116
paulokatho <pkatho@gmail.com>
2023-11-15 09:47:48 +0000

    refactor: refatorando o nome do metodo para findUserByDocument e findUserById na classe UserRepository

--- src/main/java/com/picpaysimplificado/repositories/UserRepository.java
+++ src/main/java/com/picpaysimplificado/repositories/UserRepository.java
@@ -7,6 +7,6 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByUserDocument();
-    Optional<User> findByUserId(Long id);
+    Optional<User> findUserByDocument(String document);
+    Optional<User> findUserById(Long id);
 }
