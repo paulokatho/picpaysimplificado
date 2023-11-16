290667e257283ffec6afff38a121cd8d774e4966
paulokatho <pkatho@gmail.com>
2023-11-16 12:44:30 +0000

    refactor: inserindo a anotação @Builder na classe User

--- src/main/java/com/picpaysimplificado/domain/user/User.java
+++ src/main/java/com/picpaysimplificado/domain/user/User.java
@@ -1,10 +1,7 @@
 package com.picpaysimplificado.domain.user;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 
@@ -14,6 +11,7 @@ import java.math.BigDecimal;
 @Setter
 @EqualsAndHashCode
 @AllArgsConstructor
+@Builder
 public class User {
 
     @Id
