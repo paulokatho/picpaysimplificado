64071eb22b5ddd7c8cf235804a671841801208f5
paulokatho <pkatho@gmail.com>
2023-11-16 12:50:19 +0000

    refactor: implementando os métodos de transação dos valores para a classe TransactionService

--- src/main/java/com/picpaysimplificado/service/TransactionService.java
+++ src/main/java/com/picpaysimplificado/service/TransactionService.java
@@ -1,8 +1,18 @@
 package com.picpaysimplificado.service;
 
+import com.picpaysimplificado.domain.Transaction.Transaction;
+import com.picpaysimplificado.domain.user.User;
+import com.picpaysimplificado.dtos.TransactionDTO;
 import com.picpaysimplificado.repositories.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDateTime;
+import java.util.Map;
 
 @Service
 public class TransactionService {
@@ -13,7 +23,56 @@ public class TransactionService {
     @Autowired
     private TransactionRepository repository;
 
-    public void createTransaction(TransactionDTO transaction) {
+    //RestTemplate: CLASSE QUE O SPRING NOS OFERECE PARA FAZER COMUNICAÇÕES HTTP ENTRE SERVIÇOS
+    @Autowired
+    private RestTemplate restTemplate;
 
+    @Value("${picpay.authorization.transaction}")
+    private String authorizationUrl;
+
+    public TransactionService() {
+    }
+
+    public void createTransaction(TransactionDTO transaction) throws Exception {
+
+        User sender = this.userService.findUserById(transaction.senderId());
+        User receiver = this.userService.findUserById(transaction.receiverId());
+
+        //VOU VERIFICAR SE ESSA TRANSAÇÃO SERÁ AUTORIZADA PELO PICPAY
+        userService.validateTransaction(sender, transaction.value());
+
+        boolean isAuthorized = this.authorizeTransaction();
+        if(!isAuthorized) {
+            throw new Exception("Transação não autorizada");
+        }
+
+        final var newTransaction = Transaction.builder()
+                .amount(transaction.value())
+                .sender(sender)
+                .receiver(receiver)
+                .timestamp(LocalDateTime.now())
+                .build();
+
+        sender.getBalance().subtract(transaction.value());
+        receiver.getBalance().add(transaction.value());
+
+        this.repository.save(newTransaction);
+        this.userService.saveUser(sender);
+        this.userService.saveUser(receiver);
+    }
+
+    public boolean authorizeTransaction() {
+
+        //RestTemplate: PARA FAZER REQUISIÇÕES DO TIPO GET, POST, PUT E ETC (TEM QUE FAZER A CONFIGURAÇÃO DELA)
+        //PASSAMOS A URL POR HARD CODED, MAS O IDEAL É PASSAR COMO VARIAVEL DE AMBIENTE
+        //ResponseEntity<Map> authorizationResponse = restTemplate.getForEntity("https://run.mocky.io/v3/5794d450-d2e2-4412-8131-73d0293ac1cc", Map.class);
+        ResponseEntity<Map> authorizationResponse = restTemplate.getForEntity(authorizationUrl, Map.class);
+
+        //VERFICA SE O STATUS É OK. SE FOR PEGA A KEY "MESSAGE" E VERIFICA SE O VALOR DELA É "AUTORIZADO"
+        //ESSA MENSAGEM É VERIFICADA NO SERVIÇO DO PRÓPRIO PICPAY
+        if(authorizationResponse.getStatusCode() == HttpStatus.OK) {
+            String message = (String) authorizationResponse.getBody().get("message");
+            return "Autorizado".equalsIgnoreCase(message);
+        } else return false;
     }
 }
